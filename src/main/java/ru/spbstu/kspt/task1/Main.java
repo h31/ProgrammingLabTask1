62ad8ca6fa5e3a2e33d7da215b7ff2ac593cb350
Violetta-Mamykina <violetta-1998m@mail.ru>
2018-02-18 15:31:32 +0300

    first

--- src/main/java/ru/spbstu/kspt/task1/Main.java
+++ src/main/java/ru/spbstu/kspt/task1/Main.java
@@ -1,16 +1,103 @@
 package ru.spbstu.kspt.task1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Main class
  */
 public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        logger.debug("Logging example");
-        System.out.println("Hello World!");
+    public static class Address {
+        private String street;
+        private int house;
+        private int flat;
+
+        Address(String street, int house, int flat) {
+            this.street = street;
+            this.house = house;
+            this.flat = flat;
+        }
+
+        @Override
+        public String toString() {
+            return street + " " + house + " " + flat;
+        }
+    }
+
+    public static class Person {
+        private String name;
+
+        Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    public static class addressBook {
+        private static Map<Person, Address> addressBook = new HashMap<>();
+
+        public addressBook(Map<Person, Address> myMap) {
+            this.addressBook = myMap;
+        }
+
+        public void addAddressBook(Person person, Address address) {
+            addressBook.put(person, address);
+        }
+
+        public void removeAddressBook(Person person) {
+            addressBook.remove(person);
+        }
+
+        public void changeAddress(Person person, Address address) {
+            if (addressBook.containsKey(person)) {
+                addressBook.replace(person, address);
+            }
+        }
+
+        public Address getAddress(Person person) throws IllegalAccessException {
+            if (addressBook.containsKey(person)) {
+                return addressBook.get(person);
+            } else throw new IllegalAccessException();
+        }
+
+        public void findOnStreet(String street) {
+            List<Person> listOfPerson = new ArrayList<>();
+            for (Person person : addressBook.keySet()) {
+                if (addressBook.get(person).street == street) {
+                    listOfPerson.add(person);
+                }
+            }
+        }
+
+        public void findOnHouse(Integer house) {
+            List<Person> listOfPerson = new ArrayList<>();
+            for (Person person : addressBook.keySet()) {
+                if (addressBook.get(person).house == house) {
+                    listOfPerson.add(person);
+                }
+            }
+        }
+
+        public Map<Person, Address> getAddressBook() {
+            return addressBook;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder anwser = new StringBuilder();
+            for (Person key : addressBook.keySet()) {
+                anwser.append(addressBook.get(key)).append("\n");
+            }
+            return anwser.toString();
+        }
     }
 }
+
+
--- src/test/java/ru/spbstu/kspt/task1/MainTest.java
+++ src/test/java/ru/spbstu/kspt/task1/MainTest.java
@@ -1,17 +1,48 @@
 package ru.spbstu.kspt.task1;
 
 import org.junit.jupiter.api.Test;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
+//import static ru.spbstu.kspt.task1.MainTest.createAddressBook;
+
 
 class MainTest {
-    private static final Logger logger = LogManager.getLogger(MainTest.class);
+    private static Map<Main.Person, Main.Address> MyMap = new HashMap<>();
+    private static List<Main.Person> listPerson = Arrays.asList(new Main.Person("Шадрин"),
+            new Main.Person("Дженжер"));
+    private static List<Main.Address> listAddress = Arrays.asList(new Main.Address("Новая", 12, 325),
+            new Main.Address("Пролетарская", 308, 359));
+
+    private static Main.addressBook createAddressBook() {
+        for (int i = 0; i < listPerson.size(); i++) {
+            MyMap.put(listPerson.get(i), listAddress.get(i));
+        }
+        System.out.println(MyMap);
+        return new Main.addressBook(MyMap);
+    }
 
     @Test
-    void exampleTest() {
-        logger.info("Test started");
-        assertEquals(10, 10);
-        logger.info("Test finished");
+    void addAddressBook() {
+        Main.addressBook addressBook = createAddressBook();
+        addressBook.addAddressBook(new Main.Person(""), new Main.Address("", 8, 9));
+        assertEquals(3, addressBook.getAddressBook().size());
+    }
+
+    @Test
+    void getAddress() {
+        Main.addressBook addressBook = createAddressBook();
+        System.out.println(addressBook);
+        try {
+            assertEquals(new Main.Address("Новая", 12, 325).toString(),
+                    addressBook.getAddress(new Main.Person("Дженжер")).toString());
+        } catch (IllegalAccessException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+
