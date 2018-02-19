5e20cf97260bb72861723c13a142ccd68c50cdaa
DaniilPavlov <cool.sharagin@mail.ru>
2018-02-19 19:32:23 +0300

    1

--- src/main/java/ru/spbstu/kspt/task1/Main.java
+++ src/main/java/ru/spbstu/kspt/task1/Main.java
@@ -6,11 +6,34 @@ import org.apache.logging.log4j.Logger;
 /**
  * Main class
  */
-public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.class);
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        logger.debug("Logging example");
-        System.out.println("Hello World!");
+public class Main {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        BigBigInt bbInt1;
+        BigBigInt bbInt2;
+
+        do {
+            System.out.println("Введите ПЕРВОЕ число из интервала [0,+oo]");
+            String string1 = in.nextLine();
+            bbInt1 = new BigBigInt(string1);
+        }while (bbInt1.getValue() == null);
+
+        do {
+            System.out.println("Введите ВТОРОЕ число из интервала [0,+oo]");
+            String string2 = in.nextLine();
+            bbInt2 = new BigBigInt(string2);
+        }while (bbInt2.getValue() == null);
+
+        System.out.println("Первое число: " + bbInt1.getValue());
+        System.out.println("Второе число: " + bbInt2.getValue());
+        System.out.println("Сравнение двух чисел:           " + bbInt1.Comparison(bbInt2));
+        System.out.println("Сумма двух чисел:               " + BigBigInt.addition(bbInt1, bbInt2).getValue());
+        System.out.println("Разность двух чисел:            " + BigBigInt.subtraction(bbInt1, bbInt2).getValue());
+        System.out.println("Произведение двух чисел:        " + BigBigInt.multiplication(bbInt1, bbInt2).getValue());
+        System.out.println("Деление двух чисел:             " + BigBigInt.division(bbInt1, bbInt2).getValue());
+        //System.out.println("Остаток от деления двух чисел:  " + BigBigInt.residue(bbInt1, bbInt2).getValue());
     }
 }
