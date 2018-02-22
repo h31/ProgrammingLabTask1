2c1cf17caeb8234884ea2676b974e865ad1547d5
vladislav <Vlud5@mail.ru>
2018-02-22 21:26:51 +0300

    Task1.Plus,toString,equals,minus.

--- src/main/java/ru/spbstu/kspt/task1/Polinom.java
+++ src/main/java/ru/spbstu/kspt/task1/Polinom.java
@@ -1,6 +1,5 @@
 package ru.spbstu.kspt.task1;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Polinom {
 
@@ -16,7 +15,7 @@ public class Polinom {
         String[] parts = this.function.split(";");
         for (int i = 0; i < parts.length; i++) {
             if (!parts[i].isEmpty())
-            this.coef.add( Integer.parseInt(parts[i]));
+                this.coef.add(Integer.parseInt(parts[i]));
         }
     }
 
@@ -27,11 +26,9 @@ public class Polinom {
 
     private Polinom equ(Polinom pol) {
         Polinom result = this.min(pol);
-        int difference = Math.abs(this.order - pol.order);
-        for (int i = 0; i < difference; i++) {
+        for (int i = 0; i < Math.abs(this.order - pol.order); i++) {
             result.coef.add(i, 0);
         }
-        result.order = result.coef.size() - 1;
         result.function = result.toString();
         return result;
     }
@@ -45,7 +42,24 @@ public class Polinom {
             result.coef.add(i, (maxPol.coef.get(i) + minPol.coef.get(i)));
         }
         result.function = result.toString();
-        System.out.println(result);
+        return result;
+    }
+
+    public Polinom minus(Polinom pol) {
+        Polinom minuend, subtrahend;
+        if (this.order > pol.order) {
+            minuend = this;
+            subtrahend = this.equ(pol);
+        } else {
+            minuend = this.equ(pol);
+            subtrahend = pol;
+        }
+        int end = Math.max(this.order, pol.order);
+        Polinom result = new Polinom(end, "");
+        for (int i = 0; i <= end; i++) {
+            result.coef.add(i, (minuend.coef.get(i) - subtrahend.coef.get(i)));
+        }
+        result.function = result.toString();
         return result;
     }
 
@@ -57,6 +71,31 @@ public class Polinom {
         }
         result.append(this.coef.get(this.order));
         return result.toString();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Polinom pol = (Polinom) object;
+        if (object.getClass() != this.getClass() || this.order != pol.order) return false;
+        int a, b;
+        Polinom maxPol, minPol;
+        if (this.coef.get(0) > pol.coef.get(0)) {
+            maxPol = this;
+            minPol = pol;
+        } else {
+            maxPol = pol;
+            minPol = this;
+        }
+        if ((maxPol.coef.get(0) % minPol.coef.get(0)) == 0) {
+            a = maxPol.coef.get(0) / minPol.coef.get(0);
+        } else return false;
+        for (int i = 1; i <= 4; i++) {
+            if ((maxPol.coef.get(i) % minPol.coef.get(i)) == 0)
+                b = maxPol.coef.get(i) / minPol.coef.get(i);
+            else return false;
+            if (a != b) return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
--- src/test/java/ru/spbstu/kspt/task1/PolinomTest.java
+++ src/test/java/ru/spbstu/kspt/task1/PolinomTest.java
@@ -5,14 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PolinomTest {
-
     @Test
     void plus() {
         assertEquals(new Polinom(4,"7;3;-3;-3;-8"),
                 new Polinom(4, "7;3;-6;-1;-8").plus(new Polinom(2, "3;-2;0")));
     }
 
-
-
-
+    @Test
+    void minus() {
+        assertEquals(new Polinom(4,"7;3;-9;1;-8"),
+                new Polinom(4, "7;3;-6;-1;-8").minus(new Polinom(2, "3;-2;0")));
+    }
 }
