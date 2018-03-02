547a6cff40059b9a0f940156c48f0d373fd085c8
evgenysobko <jooohn127@gmail.com>
2018-03-02 10:16:29 +0300

    Minor update Changelog: -a lot of bugs with rotate side -fix rotate cube

--- src/main/java/myClass/Cube.java
+++ src/main/java/myClass/Cube.java
@@ -3,11 +3,11 @@ package myClass;
 import java.util.Arrays;
 
 public class Cube {
-    private int n;
     private char[][][] cube;
+    private char[][] arr;
+    private int n;
 
     char[] symbols = {'W', 'R', 'B', 'O', 'Y', 'G'};
-    char[] symbolsAdd = {' ', ' ', ' ', ' ', ' ', ' '};
 
     Cube(int n) {
         this.n = n;
@@ -24,69 +24,92 @@ public class Cube {
         return cube;
     }
 
-    public void RotateLeft() {
-        symbolsAdd[5] = symbols[1];
-        symbolsAdd[1] = symbols[2];
-        symbols[1] = symbolsAdd[1];
-        symbolsAdd[2] = symbols[3];
-        symbols[2] = symbolsAdd[2];
-        symbolsAdd[3] = symbols[5];
-        symbols[3] = symbolsAdd[3];
-        symbols[5] = symbolsAdd[5];
-        for (int i = 0; i < 6; ++i) {
+    int x = 0, y = 0;
+
+    public void RotateOpposite() {
+        for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                Arrays.fill(cube[i][j], symbols[i]);
+                cube[x][i][j] = cube[x][j][i];
+                cube[y][i][j] = cube[y][j][i];
             }
         }
+    }
+
+    char[][] help = new char[0][];
+    char[][] help1 = new char[0][];
+
+    public void RotateLeft() {
+        y = 5;
+        RotateOpposite();
+        help = cube[1];
+        help1 = cube[5];
+        cube[5] = help;
+        help = cube[2];
+        cube[1] = help;
+        help = cube[3];
+        cube[2] = help;
+        cube[3] = help1;
     }
 
     public void RotateRight() {
-        symbolsAdd[1] = symbols[5];
-        symbolsAdd[2] = symbols[1];
-        symbolsAdd[3] = symbols[2];
-        symbolsAdd[5] = symbols[3];
-        symbols[1] = symbolsAdd[1];
-        symbols[2] = symbolsAdd[2];
-        symbols[3] = symbolsAdd[3];
-        symbols[5] = symbolsAdd[5];
-        for (int i = 0; i < 6; ++i) {
-            for (int j = 0; j < n; ++j) {
-                Arrays.fill(cube[i][j], symbols[i]);
-            }
-        }
+        RotateLeft();
+        RotateLeft();
+        RotateLeft();
     }
 
     public void RotateUp() {
-        symbolsAdd[0] = symbols[2];
-        symbolsAdd[2] = symbols[4];
-        symbolsAdd[5] = symbols[0];
-        symbolsAdd[4] = symbols[5];
-        symbols[0] = symbolsAdd[0];
-        symbols[2] = symbolsAdd[2];
-        symbols[4] = symbolsAdd[4];
-        symbols[5] = symbolsAdd[5];
-        for (int i = 0; i < 6; ++i) {
-            for (int j = 0; j < n; ++j) {
-                Arrays.fill(cube[i][j], symbols[i]);
-            }
-        }
+        x = 1;
+        y = 3;
+        RotateOpposite();
+        help1 = cube[0];
+        help = cube[5];
+        cube[5] = help1;
+        help1 = cube[2];
+        cube[0] = help1;
+        help1 = cube[4];
+        cube[2] = help1;
+        cube[4] = help;
     }
 
     public void RotateDown() {
-        symbolsAdd[0] = symbols[5];
-        symbolsAdd[2] = symbols[0];
-        symbolsAdd[5] = symbols[4];
-        symbolsAdd[4] = symbols[2];
-        symbols[0] = symbolsAdd[0];
-        symbols[2] = symbolsAdd[2];
-        symbols[4] = symbolsAdd[4];
-        symbols[5] = symbolsAdd[5];
-        for (int i = 0; i < 6; ++i) {
+        RotateUp();
+        RotateUp();
+        RotateUp();
+    }
+
+    public void SpinFront() {
+        char[][] help = cube[2].clone();
+        for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                Arrays.fill(cube[i][j], symbols[i]);
+                cube[2][i][j] = help[j][n - i - 1];
             }
         }
+        char[] h = cube[0][n - 1].clone();
+        char c;
+        for (int i = 0; i < n; ++i) {
+            cube[0][n - 1][i] = cube[1][n - i - 1][n - 1];
+        }
+        for (int i = 0; i < n; ++i) {
+            c = cube[3][i][0];
+            cube[3][i][0] = h[i];
+            h[i] = c;
+        }
+        for (int i = 0; i < n; ++i) {
+            c = cube[4][0][i];
+            cube[4][0][i] = h[n - i - 1];
+            h[n - i - 1] = c;
+        }
+        for (int i = 0; i < n; ++i) {
+            cube[1][i][n - 1] = h[n - i - 1];
+        }
+    }
+
+    public void SpinRight() {
+        RotateRight();
+        SpinFront();
+        RotateLeft();
     }
 }
+
 
 
--- src/main/java/myClass/Main.java
+++ src/main/java/myClass/Main.java
@@ -6,64 +6,44 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Введите размер кубика: ");
+        System.out.print("Enter cube size: ");
         int n = reader.nextInt();
 
         Cube cube = new Cube(n);
 
         PrintCube prntC = new PrintCube(n);
         prntC.Print(cube);
-        int k = 0;
-        do {
-            System.out.println("Что будем с ним делать?");
+        while (true) {
+            System.out.println("What'll we do?");
             Scanner look = new Scanner(System.in);
             String l = look.nextLine();
-            if (Objects.equals(l, "Закончить")) {
+            if (Objects.equals(l, "Print")) {
+                PrintCube printC1 = new PrintCube(n);
+                printC1.Print(cube);
+            }
+            if (Objects.equals(l, "Stop")) {
                 System.out.println("Досвидули))0)");
-                k = k - 1000000;
+                break;
             }
-            if (Objects.equals(l, "Влево")) {
+            if (Objects.equals(l, "Left")) {
                 cube.RotateLeft();
-                System.out.println("Кубик повернут влево. Вывести на экран?");
-                Scanner look1 = new Scanner(System.in);
-                String k1 = look1.nextLine();
-                if (Objects.equals(k1, "Да")) {
-                    PrintCube printC1 = new PrintCube(n);
-                    printC1.Print(cube);
-                }
             }
-            if (Objects.equals(l, "Вправо")) {
+            if (Objects.equals(l, "Right")) {
                 cube.RotateRight();
-                System.out.println("Кубик повернут вправо. Вывести на экран?");
-                Scanner look2 = new Scanner(System.in);
-                String k2 = look2.nextLine();
-                if (Objects.equals(k2, "Да")) {
-                    PrintCube printC1 = new PrintCube(n);
-                    printC1.Print(cube);
-                }
             }
-            if (Objects.equals(l, "Вверх")) {
+            if (Objects.equals(l, "Up")) {
                 cube.RotateUp();
-                System.out.println("Кубик повернут вверх. Вывести на экран?");
-                Scanner look3 = new Scanner(System.in);
-                String k3 = look3.nextLine();
-                if (Objects.equals(k3, "Да")) {
-                    PrintCube printC1 = new PrintCube(n);
-                    printC1.Print(cube);
-                }
             }
-            if (Objects.equals(l, "Вниз")) {
+            if (Objects.equals(l, "Down")) {
                 cube.RotateDown();
-                System.out.println("Кубик повернут вниз. Вывести на экран?");
-                Scanner look4 = new Scanner(System.in);
-                String k4 = look4.nextLine();
-                if (Objects.equals(k4, "Да")) {
-                    PrintCube printC1 = new PrintCube(n);
-                    printC1.Print(cube);
-                }
             }
-            ++k;
-        } while (k >= 0);
+            if (Objects.equals(l, "F")) {
+                cube.SpinFront();
+            }
+            if (Objects.equals(l, "R")) {
+                cube.SpinRight();
+            }
+        }
     }
 }
 
