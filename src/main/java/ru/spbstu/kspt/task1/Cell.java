5dcf532f641075aef88c90452645777f2b4337d4
Denispok <vfkmxbr23@yandex.ru>
2018-03-10 15:02:33 +0300

    TicTacField 1.1.2 + Cell code simplifying

--- src/main/java/ru/spbstu/kspt/task1/Cell.java
+++ src/main/java/ru/spbstu/kspt/task1/Cell.java
@@ -1,43 +1,7 @@
 package ru.spbstu.kspt.task1;
 
-import static ru.spbstu.kspt.task1.Cell.State.*;
-
-public class Cell {
-
-    public enum State {
-        EMPTY,
-        NOUGHT,
-        CROSS;
-    }
-
-    private State state;
-
-    public Cell() {
-        state = EMPTY;
-    }
-
-    public void setState(State state) {
-        if (state == EMPTY || state == NOUGHT || state == CROSS) {
-            this.state = state;
-        } else throw new IllegalArgumentException("illegal state of cell");
-    }
-
-    public State getState() {
-        return state;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) return true;
-        if (other == null || getClass() != other.getClass()) return false;
-
-        Cell cell = (Cell) other;
-
-        return state == cell.state;
-    }
-
-    @Override
-    public int hashCode() {
-        return state != null ? state.hashCode() : 0;
-    }
+public enum Cell {
+    EMPTY,
+    NOUGHT,
+    CROSS;
 }
--- src/main/java/ru/spbstu/kspt/task1/TicTacToeField.java
+++ src/main/java/ru/spbstu/kspt/task1/TicTacToeField.java
@@ -3,7 +3,8 @@ package ru.spbstu.kspt.task1;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static ru.spbstu.kspt.task1.Cell.State.*;
+import static ru.spbstu.kspt.task1.Cell.*;
+
 
 public class TicTacToeField {
     private final int size;
@@ -17,14 +18,14 @@ public class TicTacToeField {
 
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
-                mField[row][col] = new Cell();
+                mField[row][col] = EMPTY;
             }
         }
     }
 
-    private void changeState(int row, int col, Cell.State state) {
+    private void changeState(int row, int col, Cell cell) {
         if (!cellOnField(row, col)) throw new IllegalArgumentException("it's not such cell on the mField");
-        mField[row][col].setState(state);
+        mField[row][col] = cell;
     }
 
     public void addCross(int row, int col) {
@@ -51,15 +52,16 @@ public class TicTacToeField {
             new CellPosition(-1, 1), new CellPosition(0, 1), new CellPosition(1, 0), new CellPosition(1, 1)
     };
 
-    private ArrayList<CellPosition> longestSequence(Cell.State state) {
+    private ArrayList<CellPosition> longestSequence(Cell cell) {
         ArrayList<CellPosition> result = new ArrayList<>();
+
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
                 for (CellPosition dir : DIRECTIONS) {
                     CellPosition currentCellPosition = new CellPosition(row, col);
                     ArrayList<CellPosition> currentSequence = new ArrayList<CellPosition>();
 
-                    while (currentCellPosition.getCell(mField).getState() == state) {
+                    while (currentCellPosition.getCell(mField) == cell) {
                         currentSequence.add(currentCellPosition);
                         CellPosition nextCellPosition = currentCellPosition.shift(dir);
 
@@ -114,7 +116,7 @@ public class TicTacToeField {
         StringBuilder stringBuilder = new StringBuilder();
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
-                switch (mField[row][col].getState()) {
+                switch (mField[row][col]) {
                     case EMPTY:
                         stringBuilder.append(' ');
                         break;
