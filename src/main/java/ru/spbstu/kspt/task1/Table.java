ee5fd97d3af82abc66907129997e267154eaf5df
Yulia <myk.99@yandex.ru>
2018-03-11 23:48:26 +0300

    Переделала на TreeMap. Сегодня-завтра доделаю бинарный поиск.

--- src/main/java/ru/spbstu/kspt/task1/Table.java
+++ src/main/java/ru/spbstu/kspt/task1/Table.java
@@ -1,111 +1,69 @@
-package ru.spbstu.kspt.task1;
+package main.java.ru.spbstu.kspt.task1;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Main class
  */
 public class Table {
-    public void addAPair(double x, double y) {
-        if (!isExist(x))
-            wr.add(new Pair(x, y));
-        else
-            throw new IllegalArgumentException();
-    }
-    ArrayList<Pair> wr = new ArrayList<Pair>();
+    TreeMap<Double, Double> pairMap = new TreeMap<Double, Double>(); // добавить пару
+    private ArrayList<Object> difference;
 
-    public void addPair(double x) {
-        if (!isExist(x)) {
-            double y = polynomNewton(x);
-            wr.add(new Pair(x, y));
-        } else
-            throw new IllegalArgumentException();
+    public void addValue(double x, double y) {
+        pairMap.put(x, y);
     }
 
-    public Pair search(double x, double y) {
-        for (Pair p : wr)
-            if (Math.abs(p.x - x) < 1e-5 && Math.abs(p.y - y) < 1e-5)
-                return p;
-        return null;
+    public Double funValue(double x) { // вывести для х у
+        return pairMap.get(x);
     }
 
-    public void deletPair(double x, double y) {
-        Pair tmp = search(x, y);
-        if (tmp != null)
-            wr.remove(tmp);
-        else
-            throw new IllegalArgumentException();
+    public void dellValue(double x) { // удалить пару
+        pairMap.remove(x);
     }
 
-    public String showTable() {
-        String res = "";
-        for (Pair p : wr) {
-            res += (p.x + " " + p.y + "\n");
+    public String showTable(){
+        StringBuilder res = new StringBuilder();
+        for (Map.Entry e:pairMap.entrySet()) {
+            res.append(e.getKey()).append(" ").append(e.getValue()).append("\n");
         }
-        return res;
+        return res.toString();
     }
 
-    public void pairSearhByX(double x0) {
-        Pair res = null;
-        double min = Double.MAX_VALUE;
-        for (Pair i : wr
-                ) {
-            if (i.dif(x0) <= min) {
-                min = i.dif(x0);
-                res = i;
+    public String findClosestPair(double x0) {// найти ближайшее значени для х0
+        double diff = Double.MAX_VALUE;
+        double res = 0;
+        for (Map.Entry<Double, Double> entry : pairMap.entrySet())
+            if (diff > Math.abs(entry.getKey() - x0)) {
+                diff = Math.abs(entry.getKey() - x0);
+                res = entry.getKey();
             }
-        }
-        System.out.println(res.x + " " + res.y);
+        return res + " " + pairMap.get(res);
     }
 
-    private boolean isExist(double x) {
-        for (Pair p : wr) {
-            if (Math.abs(p.x - x) < 1e-5) return true;
+    private ArrayList<Double> splitDifference() {
+        int n = pairMap.size();
+        ArrayList<Double> difference = new ArrayList();
+        for (Map.Entry<Double, Double> entry : pairMap.entrySet()) {
+            difference.add(entry.getValue());
         }
-        return false;
-    }
-
-    public double polynomNewton(double x) {
-        int n = wr.size();
-        double[][] difference = splitDifference();
-        double xCoeff = 1;
-        double y = difference[0][1];
-        for (int i = 0; i <= n; i++) {
-            xCoeff = xCoeff * (x - wr.get(i).x);
-            y = y + xCoeff * difference[0][i + 2];
-        }
-        return y;
-    }
-
-    private double[][] splitDifference() {
-        int n = wr.size();
-        double[][] difference = new double[n][];
-        for (int i = 0; i <= n; i++) {
-            difference[0] = new double[n - 1];
-            difference[i][0] = wr.get(0).x;
-            difference[i][1] = wr.get(0).y;
-        }
-        for (int i = 1; i <= n + 1; i++) {
-            for (int j = 0; j <= n - 1; i++) {
-                double xDiff = difference[j + i][0] - difference[j][0];
-                double yDiff = difference[j + 1][i] - difference[j][i];
-                difference[j][i] = yDiff / xDiff;
+        for (int j = 1; j < n; j++) {
+            for (int i = n - 1; i > j - 1; i--) {
+                difference.set(i, (difference.get(i) - difference.get(i - 1)) /
+                        ((Double) pairMap.values().toArray()[i] - (Double) pairMap.values().toArray()[i - j]));
             }
         }
         return difference;
     }
 
-    static class Pair {
-        public double x;
-        public double y;
-
-        public Pair(double x, double y) {
-            this.x = x;
-            this.y = y;
+    public double calculateNewtonPolynomialValue(double x0) {
+        ArrayList<Double> difference = this.splitDifference();
+        int n = difference.size() - 1;
+        double fun = difference.get(n);
+        for (int i = n - 1; i > -1; i--) {
+            fun = fun * (x0 - (Double) (pairMap.values().toArray()[i])) + difference.get(n);
         }
-
-        public double dif(double x0) {
-            return Math.abs(x0 - x);
-        }
+        return fun;
     }
 }
