4681c4e2152918cb7ddd18c0e4a3dbb8673fc834
AlexandraTrubkina <sashatru99@mail.ru>
2018-03-12 22:08:10 +0300

    quaternion

--- src/test/java/ru/spbstu/kspt/task1/MainTest.java
+++ src/test/java/ru/spbstu/kspt/task1/MainTest.java
@@ -5,13 +5,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import static org.junit.jupiter.api.Assertions.*;
 
-class MainTest {
+
+class MainTest{
     private static final Logger logger = LogManager.getLogger(MainTest.class);
 
     @Test
-    void exampleTest() {
-        logger.info("Test started");
-        assertEquals(10, 10);
-        logger.info("Test finished");
+    void abs(){
+        assertEquals( 8.602325267042627, new Main.Quaternion(2.0, 3.0, 5.0, 6.0).abs());
+        assertEquals(9.670056876771719, new Main.Quaternion(2.1, 3.5, 4.7, 7.4).abs());
+        assertEquals(8.710074626545975, new Main.Quaternion(2.25,3.86,4.92,5.63).abs());
     }
 }
