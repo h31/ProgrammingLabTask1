732a3953df91caaee91c118ad9e849f511ab0d5f
Yulia <myk.99@yandex.ru>
2018-03-14 22:33:05 +0300

    Реализовала toString,equals и hash

--- src/main/java/ru/spbstu/kspt/task1/Table.java
+++ src/main/java/ru/spbstu/kspt/task1/Table.java
@@ -2,6 +2,7 @@ package main.java.ru.spbstu.kspt.task1;
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Objects;
 import java.util.TreeMap;
 
 /**
@@ -32,26 +33,27 @@ public class Table {
     }
 
     public String findClosestPair(double x0) {// найти ближайшее значени для х0
-        double diff = Double.MAX_VALUE;
-        double res = 0;
-        for (Map.Entry<Double, Double> entry : pairMap.entrySet())
-            if (diff > Math.abs(entry.getKey() - x0)) {
-                diff = Math.abs(entry.getKey() - x0);
-                res = entry.getKey();
-            }
-        return res + " " + pairMap.get(res);
+//        double diff = Double.MAX_VALUE;
+//        double res = 0;
+//        for (Map.Entry<Double, Double> entry : pairMap.entrySet())
+//            if (diff > Math.abs(entry.getKey() - x0)) {
+//                diff = Math.abs(entry.getKey() - x0);
+//                res = entry.getKey();
+//            }
+//        return res + " " + pairMap.get(res);
+          return "";
     }
 
     private Double[] diff() {
         int n = pairMap.size();
-        Double[] a = new Double[n];
+        Double[] diff = new Double[n];
         for (int i = 0; i < n; i++)
-            a[i] = (Double) pairMap.values().toArray()[i];
+            diff[i] = (Double) pairMap.values().toArray()[i];
         for (int j = 1; j < n; j++)
             for (int i = n - 1; i > j - 1; i--)
-                    a[i] = (a[i] - a[i - 1])
+                    diff[i] = (diff[i] - diff[i - 1])
                             /((Double)pairMap.keySet().toArray()[i] - (Double)pairMap.keySet().toArray()[i - j]);
-        return a;
+        return diff;
     }
 
     public Double calculateNewtonPolynom(double x0) {
@@ -61,5 +63,28 @@ public class Table {
         for (int i = n - 1; i > -1;  i--)
             temp = temp * (x0 - (Double) pairMap.keySet().toArray()[i]) + a[i];
         return temp;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        for(Map.Entry e : pairMap.entrySet()){
+            res.append("x: ").append(e.getKey()).append(" y: ").append(e.getValue()).append("\n");
+        }
+        return res.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Table table = (Table) o;
+        return Objects.equals(pairMap, table.pairMap) &&
+                Objects.equals(difference, table.difference);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pairMap, difference);
     }
 }
