aee9f96e81b143f459badb4a527a82c15e47cab6
vladislav <Vlud5@mail.ru>
2018-03-17 09:03:45 +0300

    Task1. SummarizeOrSubtract(Во избежание дублирования кода два метода определены в один, по запросу request выбираем, какой из методов нам нужен). Evaluate. Multiply. Divide (Аналогично SummarizeOrSubstract были объединены два метода в один. Создан вспомагательный метод для избавления остатка от незначащих нулей и придания ему адекватного вида)

--- src/test/java/ru/spbstu/kspt/task1/PolinomialTest.java
+++ src/test/java/ru/spbstu/kspt/task1/PolinomialTest.java
@@ -24,4 +24,16 @@ class PolinomialTest {
         assertEquals(new Polinomial(new int[]{0,16,-18,-3,-15,-5,21}),
                 new Polinomial(new int[]{-8,-3,-3,3,7}).multiply(new Polinomial(new int[]{0,-2,3})));
     }
+    @Test
+    void divide() {
+        assertEquals(new Polinomial(new int[]{0,-1,-2}),
+                new Polinomial(new int[]{-8,-3,-7,3,-7}).divide(new Polinomial(new int[]{4,-2,3}), true));
+        assertEquals(new Polinomial(new int[]{-10}),
+                new Polinomial(new int[]{-8,-3,-7,3,-7}).divide(new Polinomial(new int[]{4,-2,3}), false));
+        assertEquals(new Polinomial(new int[]{-8,1}),
+                new Polinomial(new int[]{-3,-22,23,-10,2}).
+                        divide(new Polinomial(new int[]{5,-3,1}), false));
+        assertEquals(new Polinomial(new int[]{0}),
+                new Polinomial(new int[]{8,-6,-5,3}).divide(new Polinomial(new int[]{-4,1,3}), false));
+    }
 }
