71702af591cda8f67a08b76e8c104d0158a40022
vladislav <Vlud5@mail.ru>
2018-03-17 06:28:47 +0300

    Task1. SummarizeOrSubtract(Во избежание дублирования кода два метода определены в один, по запросу request выбираем, какой из методов нам нужен). Evaluate. Multiply.

--- src/main/java/ru/spbstu/kspt/task1/Polinomial.java
+++ src/main/java/ru/spbstu/kspt/task1/Polinomial.java
@@ -1,8 +1,5 @@
 package ru.spbstu.kspt.task1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Polinomial {
 
     private int[] function;
@@ -13,32 +10,19 @@ public class Polinomial {
         this.degree = function.length - 1;
     }
 
-    public Polinomial summarizeOrSubtract(Polinomial pol, String request) {
-        if (!request.equals("Summarize") && !request.equals("Subtract"))
-                throw new IllegalArgumentException("Неверный запрос: введите Summarize или Subtract");
+    public Polinomial summarizeOrSubtract(Polinomial pol, boolean request) {
         int maxDegree = Math.max(this.degree, pol.degree);
         int minDegree = Math.min(this.degree, pol.degree);
-        int[] resultFunction = new int[maxDegree + 1];
-        if (request.equals("Summarize")) {
-            for (int i = 0; i <= maxDegree; i++) {
-                if (i <= minDegree) {
-                    resultFunction[i] = this.function[i] + pol.function[i];
-                } else {
-                    if (this.function.length > minDegree) resultFunction[i] = this.function[i];
-                    else resultFunction[i] = pol.function[i];
-                }
-            }
-        } else {
-            for (int i = 0; i <= maxDegree; i++) {
-                if (i <= minDegree) {
-                    resultFunction[i] = this.function[i] - pol.function[i];
-                } else {
-                    if (this.function.length > minDegree) resultFunction[i] = this.function[i];
-                    else resultFunction[i] = -pol.function[i];
-                }
+        int[] resultOfFunction = new int[maxDegree + 1];
+        for (int i = 0; i <= maxDegree; i++) {
+            if (i <= minDegree) {
+                resultOfFunction[i] = this.function[i] + (request ? pol.function[i] : - pol.function[i]);
+            } else {
+                if (this.function.length > minDegree) resultOfFunction[i] = this.function[i];
+                else resultOfFunction[i] = request ? pol.function[i] : - pol.function[i];
             }
         }
-        return new Polinomial(resultFunction);
+        return new Polinomial(resultOfFunction);
     }
 
     public int evaluate(int x) {
@@ -49,11 +33,21 @@ public class Polinomial {
         return result;
     }
 
+    public Polinomial multiply(Polinomial pol) {
+        int[] resultOfFunction = new int[this.degree + pol.degree + 1];
+        for (int i = this.degree; i >= 0; i--) {
+            for (int j = pol.degree; j >= 0; j--) {
+                resultOfFunction[i + j] = this.function[i] * pol.function[j] + resultOfFunction[i + j];
+            }
+        }
+        return new Polinomial(resultOfFunction);
+    }
+
     @Override
     public boolean equals(Object object) {
         Polinomial pol = (Polinomial) object;
         if (object.getClass() != this.getClass() || this.degree != pol.degree) return false;
-        double a,b;
+        double a, b;
         if (this.function[this.degree] % pol.function[this.degree] == 0) {
             a = this.function[this.degree] % pol.function[this.degree];
         } else return false;
@@ -62,8 +56,7 @@ public class Polinomial {
                 if (this.function[i] % pol.function[i] == 0) {
                     b = this.function[i] % pol.function[i];
                     if (a != b) return false;
-                }
-                else if(this.function[i] != 0) return false;
+                } else if (this.function[i] != 0) return false;
             }
         }
         return true;
--- src/test/java/ru/spbstu/kspt/task1/PolinomialTest.java
+++ src/test/java/ru/spbstu/kspt/task1/PolinomialTest.java
@@ -9,15 +9,19 @@ class PolinomialTest {
     void summarizeOrSubtract() {
         assertEquals(new Polinomial(new int[]{1,2,3,-1,2,1}),
                 new Polinomial(new int[]{-4,3,-1,1,2,1}).
-                        summarizeOrSubtract(new Polinomial(new int[]{5,-1,4,-2}), "Summarize"));
+                        summarizeOrSubtract(new Polinomial(new int[]{5,-1,4,-2}), true));
         assertEquals(new Polinomial(new int[]{-9,4,-5,-3,2,1}),
                 new Polinomial(new int[]{-4,3,-1,1,2,1}).
-                        summarizeOrSubtract(new Polinomial(new int[]{5,-1,4,-2}), "Subtract"));
+                        summarizeOrSubtract(new Polinomial(new int[]{5,-1,4,-2}), false));
     }
     @Test
     void evaluate() {
         assertEquals(102, new Polinomial(new int[]{-8,-1,-6,3,7}).evaluate(2));
         assertEquals(7, new Polinomial(new int[]{7}).evaluate(2));
-
+    }
+    @Test
+    void multiply() {
+        assertEquals(new Polinomial(new int[]{0,16,-18,-3,-15,-5,21}),
+                new Polinomial(new int[]{-8,-3,-3,3,7}).multiply(new Polinomial(new int[]{0,-2,3})));
     }
 }
