acd1e883164ee4f9b2801b436c40ac6d1e44061f
yongzhengzhao <happyguyszyz@gmail.com>
2018-03-24 20:11:51 +0300

    Чжао Юнчжэн, целочисленный полином

--- src/main/java/task01/PolynomialTest.java
+++ src/main/java/task01/PolynomialTest.java
@@ -1,28 +1,28 @@
 package task01;
 
-public class PolynTest {
+public class PolynomialTest {
     public static void main(String[] args) {
         int ar[] = new int[]{2, 1, 6, 0};
-        Polyn p = new Polyn(ar);
+        Polynomial p = new Polynomial(ar);
         System.out.print("p: ");
         p.print();
-        int ar1[] = new int[]{0, -2, 4};
-        Polyn p1 = new Polyn(ar1);
+        int ar1[] = new int[]{0,-2, 4};
+        Polynomial p1 = new Polynomial(ar1);
         System.out.print("p1: ");
         p1.print();
-        Polyn res = p.sum(p1);
+        Polynomial res = p.sum(p1);
         System.out.print("p + p1 = ");
         res.print();
-        Polyn res1 = p.sub(p1);
+        Polynomial res1 = p.sub(p1);
         System.out.print("p - p1 = ");
         res1.print();
-        Polyn res2 = p.mul(p1);
+        Polynomial res2 = p.mult(p1);
         System.out.print("p * p1 = ");
         res2.print();
-        Polyn res3 = p.div(p1);
+        Polynomial res3 = p.div(p1);
         System.out.print("p / p1 = ");
         res3.print();
-        Polyn res4 = p.remainder(p1);
+        Polynomial res4 = p.remainder(p1);
         System.out.print("p % p1 = ");
         res4.print();
     }
