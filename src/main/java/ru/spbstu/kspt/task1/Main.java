797182e14df8720600c285f0cd89cc43a9977a12
Farrukh <farrukh.sharipov99@mail.ru>
2018-03-26 15:25:47 +0300

    почти закончил

--- src/main/java/ru/spbstu/kspt/task1/Main.java
+++ src/main/java/ru/spbstu/kspt/task1/Main.java
@@ -1,16 +1,76 @@
 package ru.spbstu.kspt.task1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+class Node {
+    int date;
+    Node left;
+    Node right;
+}
 
-/**
- * Main class
- */
-public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.class);
+class BST {
+    public Node creatNewNode(int k) {
+        Node a = new Node();
+        a.date = k;
+        a.left = null;
+        a.right = null;
+        return a;
+    }
 
-    public static void main(String[] args) {
-        logger.debug("Logging example");
-        System.out.println("Hello World!");
+    public Node insert(Node node, int val) {
+        if (node == null) {
+            return creatNewNode(val);
+        }
+        if (val < node.date) {
+            node.left = insert(node.left, val);
+        } else if (val > node.date) node.right = insert(node.right, val);
+        return node;
+    }
+
+    public Node deleteElement(Node node, int key) {
+        while (node != null) {
+            if (node.date == key) {
+                if (node.left == null && node.right == null) node = null;
+
+            }
+            else if (node.date < key) node = node.left;
+            else if (node.date > key) node = node.right;
+        }
+        return node;
+    }
+
+
+    public boolean searchElement(Node node, int key) {
+        while (node != null) {
+            if (node.date == key) return true;
+            else if (node.date > key) node = node.left;
+            else if (node.date < key) node = node.right;
+        }
+        return false;
+    }
+
+    public int rightElement(Node node, int key) {
+        while (node != null) {
+            if (node.date == key) return node.right.date;
+            else if (node.date > key) node = node.left;
+            else if (node.date < key) node = node.right;
+        }
+        return 0;
+    }
+
+    public int leftElement(Node node, int key) {
+        while (node != null) {
+            if (node.date == key) return node.left.date;
+            else if (node.date > key) node = node.left;
+            else if (node.date < key) node = node.right;
+        }
+        return 0;
+    }
+
+    public int parentElement(Node node, int key) {
+        while (node != null) {
+            if (node.right.date == key || node.left.date == key) return node.date;
+            else if (node.date > key) node = node.left;
+            else if (node.date < key) node = node.right;
+        }
+        return 0;
     }
 }
