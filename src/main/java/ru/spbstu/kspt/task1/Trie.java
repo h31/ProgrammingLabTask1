d60546b10d5503c8c67f69f24e040007ab0efb13
evgenykuzin <32079738+evgenykuzin@users.noreply.github.com>
2018-04-10 21:15:49 +0300

    доделал методы.

--- src/main/java/ru/spbstu/kspt/task1/Trie.java
+++ src/main/java/ru/spbstu/kspt/task1/Trie.java
@@ -1,20 +1,15 @@
 package ru.spbstu.kspt.task1;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.util.*;
 
 public class Trie {
-    private static final Logger logger = LogManager.getLogger(Main.class);
 
     static class TrieNode {
         Map<Character, TrieNode> children = new TreeMap<>();
         boolean branch;
     }
-    TrieNode root = new TrieNode();
 
+    TrieNode root = new TrieNode();
 
     public void insert(String str) {
         TrieNode node = root;
@@ -31,7 +26,6 @@ public class Trie {
         }
     }
 
-
     public boolean find(String str) {
         TrieNode node = root;
         for (char ch : str.toLowerCase().toCharArray()) {
@@ -44,39 +38,51 @@ public class Trie {
         return true;
     }
 
-
     public void delete(String str) {
         TrieNode node = root;
         for (char ch : str.toLowerCase().toCharArray()) {
-            if (node.branch) {
-                node = node.children.remove(ch);
-            } else {
-                node = node.children.get(ch);
+            if (node.children.get(ch) != null) {
+                if (node.branch) {
+                    node = node.children.remove(ch);
+                } else {
+                    node = node.children.get(ch);
+                }
             }
         }
     }
-    ArrayList list = new ArrayList<String>();
 
+    private ArrayList<String> list = new ArrayList<>();
 
     public ArrayList<String> search(String prefix) {
         TrieNode node = root;
         list.clear();
+        char c = ' ';
         for (char ch : prefix.toCharArray()) {
             if (node.children.containsKey(ch)) {
                 node = node.children.get(ch);
             } else {
-                logger.error("No results for: " + prefix);
-                return list;
+                throw new IllegalArgumentException("No results for: " + prefix);
             }
+            c = ch;
         }
-        StringBuilder word = new StringBuilder().append(prefix);
-        searchHelp(word, prefix, node);
+        if (!node.branch && c != ' ') {
+            for (char ch : node.children.keySet()) {
+                StringBuilder prefixBuilder = new StringBuilder(prefix);
+                if (node.children.get(ch) == null) break;
+                prefixBuilder.append(ch);
+                searchHelp(prefixBuilder, prefixBuilder.toString(), node.children.get(ch));
+            }
+        } else {
+            StringBuilder word = new StringBuilder(prefix);
+            searchHelp(word, prefix, node);
+        }
+        if (list == null) {
+           throw new IllegalArgumentException("no results for: " + prefix);
+        }
         return list;
     }
 
-
     private void searchHelp(StringBuilder word, String prefix, TrieNode node) {
-        TrieNode bazeNode = node;
         Character c = ' ';
         for (Character ch : node.children.keySet()) {
             word.append(ch);
@@ -89,49 +95,50 @@ public class Trie {
         }
     }
 
-
-    public boolean equals(Trie trie, Trie anTrie) {
-        TrieNode node1 = root;
-        TrieNode node2 = root;
-        return trie.equals(anTrie);
-    }
-
-
+    @Override
     public String toString() {
-        return toStringHelp(root, 0);
+        return toStringHelp(root, new StringBuilder(), 0);
     }
-    StringBuilder strB = new StringBuilder();
 
-    private String toStringHelp(TrieNode node, int i) {
-        StringBuilder word = new StringBuilder();
-        Character c = ' ';
+    private String toStringHelp(TrieNode node, StringBuilder word, int i) {
+
         for (Character ch : node.children.keySet()) {
-            word.append(indentation(i)).append(ch).append("\r");
-            toStringHelp(node.children.get(ch), i + 1);
-            strB = strB.append(word, 0, word.length());
+            StringBuilder strB = new StringBuilder();
+            strB.append(addSpace(i)).append(ch);
+            if (node.children != null) {
+                toStringHelp(node.children.get(ch), word, i + 1);
+                word.insert(0, strB.append("\r").toString());
+            }
+
         }
 
-        if (node.children == null) {
-            return word.toString();
-        } else return word.toString();
+
+
+        return word.toString();
     }
 
-   static Map<Integer, String> indentsMap = new HashMap<>();
+    private static Map<Integer, String> spacesMap = new HashMap<>();
 
-   public String indentation(int i){
-        String result = indentsMap.get(i);
+    private String addSpace(int i) {
+        String result = spacesMap.get(i);
         if (result == null) {
             StringBuilder indent = new StringBuilder();
             for (int k = 0; k < i; k++) {
                 indent.append(" ");
             }
             result = indent.toString();
-            indentsMap.put(i, result);
+            spacesMap.put(i, result);
         }
         return result;
-   }
+    }
 
+    public boolean equals(Trie anTrie) {
+        return this.toString().equals(anTrie.toString());
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(root);
+    }
 
 }
--- src/test/java/ru/spbstu/kspt/task1/TrieTest.java
+++ src/test/java/ru/spbstu/kspt/task1/TrieTest.java
@@ -1,17 +1,18 @@
 package ru.spbstu.kspt.task1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TrieTest {
-    private static final Logger logger = LogManager.getLogger(MainTest.class);
+
+
 
     @Test
-    void exampleTest() {
-        logger.info("Test started");
+    void insertAndDeleteTest() {
         Trie testTrie = new Trie();
         testTrie.insert("wolk");
         testTrie.insert("kotAssFirst");
@@ -22,20 +23,21 @@ public class TrieTest {
         testTrie.insert("word");
         testTrie.insert("world");
         testTrie.insert("wored");
-        assertEquals(true, testTrie.find("word"));
-        testTrie.insert("worty");
-        assertEquals(true, testTrie.find("worty"));
-        assertEquals(false, testTrie.find("hello"));
+        testTrie.insert("worry");
         testTrie.insert("kotlin");
-        assertEquals(true, testTrie.find("kotlin"));
         testTrie.insert("rrrrrrrrrr");
+        assertEquals(true, testTrie.find("word"));
+        assertEquals(true, testTrie.find("worry"));
+        assertEquals(false, testTrie.find("hello"));
+        assertEquals(true, testTrie.find("kotlin"));
+        assertEquals(true, testTrie.find("wolk"));
+        assertEquals(true, testTrie.find("kotAssFirst"));
+        assertEquals(true, testTrie.find("russian"));
+        assertEquals(true, testTrie.find("rrrrock"));
+        assertEquals(true, testTrie.find("walt"));
+        assertEquals(true, testTrie.find("roll"));
         assertEquals(true, testTrie.find("rrrrrrrrrr"));
-        testTrie.search("wor");
-        testTrie.search("kot");
-        testTrie.search("rrr");
-        testTrie.search("hello");
-        testTrie.search("wo");
-        testTrie.toString();
+        assertEquals(true, testTrie.find("kotlin"));
         testTrie.delete("world");
         assertEquals(false, testTrie.find("world"));
         testTrie.delete("rrrrock");
@@ -48,15 +50,67 @@ public class TrieTest {
         assertEquals(false, testTrie.find("roll"));
         testTrie.delete("walt");
         assertEquals(false, testTrie.find("walt"));
-        testTrie.delete("worty");
-        assertEquals(false, testTrie.find("worty"));
+        testTrie.delete("worry");
+        assertEquals(false, testTrie.find("worry"));
         assertEquals(true, testTrie.find("word"));
         testTrie.delete("kotlin");
         assertEquals(false, testTrie.find("kotlin"));
+    }
 
+    @Test
+    void searchTest() {
+        ArrayList<String> list = new ArrayList<>();
+        Trie trie = new Trie();
+        trie.insert("worry");
+        trie.insert("world");
+        trie.insert("word");
+        trie.insert("wored");
+        trie.insert("kotlin");
+        trie.insert("kotassfirst");
+        trie.insert("wolt");
+        trie.insert("wolk");
+        list.add("word");
+        list.add("wored");
+        list.add("world");
+        list.add("worry");
+        assertEquals(list ,trie.search("wor"));
+        list.clear();
+        list.add("kotassfirst");
+        list.add("kotlin");
+        assertEquals(list, trie.search("kot"));
+        list.clear();
+        list.add("wolk");
+        list.add("wolt");
+        list.add("word");
+        list.add("wored");
+        list.add("world");
+        list.add("worry");
+        assertEquals(list, trie.search("wo"));
+        list.clear();
+        try {
+            trie.search("hello");
+        }
+        catch (IllegalArgumentException e){
+            assertEquals("No results for: hello", e.getMessage());
+        }
+    }
 
+    @Test
+    void toStringTest() {
+        Trie trie = new Trie();
+        trie.insert("hello");
+        trie.insert("hey");
+        trie.insert("hola");
+        Assertions.assertEquals("h\r o\r  l\r   a\r e\r" + "  y\r" + "  l\r   l\r    o\r", trie.toString());
+    }
 
-
-        logger.info("Test finished");
+    @Test
+    void equalsTest() {
+        Trie firstTrie = new Trie();
+        Trie secondTrie = new Trie();
+        firstTrie.insert("hello");
+        secondTrie.insert("hello");
+        assertFalse(new Trie().equals(secondTrie));
+        assertTrue(firstTrie.equals(secondTrie));
     }
 }
